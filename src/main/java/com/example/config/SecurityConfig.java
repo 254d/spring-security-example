2a9ffbf4570104889057439e2f1d493b373d5801
niwase.hiroaki <niwaseh@gmail.com>
2022-11-26 22:12:50 +0900

    refs #1 非推奨の実装を改善

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,32 +1,40 @@
 package com.example.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
-  @Override
-  public void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/css/**", "/js/**").permitAll().antMatchers("/admin/**")
-        .hasAuthority("ROLE_ADMIN").anyRequest().authenticated();
-    http.formLogin().usernameParameter("username").passwordParameter("password")
-        .defaultSuccessUrl("/", true).failureUrl("/login-error");
-
-    // h2-consoleの表示不具合抑止
-    http.csrf().ignoringAntMatchers("/h2-console/**");
-    http.headers().frameOptions().disable();
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .usernameParameter("username")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/", true)
+        .failureUrl("/login-error")
+        .permitAll())
+        .logout(logout -> logout
+            .logoutSuccessUrl("/"))
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .mvcMatchers("/admin/**").hasRole("ROLE_ADMIN")
+            .anyRequest().authenticated())
+        // h2-consoleの表示不具合抑止
+        .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"))
+        .headers(headers -> headers.frameOptions().disable());
 
     // System.out.println("password : " + passwordEncoder().encode("admin1234"));
+
+    return http.build();
   }
 }
